676178e392024546845ced6d457a06e14edb618d
Desteur <fallwalkerdesteur@gmail.com>
2024-07-21 11:47:32 +0200

    add logo&about me

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,9 @@
+<!-- using data instead of src in the v-for loop allows you to access both the image source and the tag for each item in imageSources. By structuring your data as an object with properties, you can easily manage multiple attributes related to each image -->
+
 <template>
   <body>
     <main>
-      <article class="slider">
-        <img
-          src="/images/my-profile.jpg"
-          class="slider-background"
-          alt="my profile picture"
-        />
-
-        <div class="slider-content">
-          <h1>desteur piepenche fall</h1>
-          <p>developpeur web et web mobile</p>
-        </div>
-      </article>
-      <section class="about-me d-10" id="section2">
-        <div class="w20">
-          <h2>a propos de moi</h2>
-        </div>
-        <div class="w10">
-          <p>
-            <strong>Bonjour, je suis Desteur ! </strong><br />J'ai trouvé ma
-            voix en tant que Developpeur web. A ce jour je maîtrise le frontend
-            et je vise full stack pour très bientôt. En tant que personne
-            impliquée, passionnée et rigoureuse, je saurais mettre en forme vos
-            projets francophones comme anglophones. <br />A très vite pour nos
-            futures collaborations.
-          </p>
-        </div>
-      </section>
+      <AboutView></AboutView>
       <section id="section1" class="exper">
         <div class="w20 mz-11">
           <h2>mes creations</h2>
@@ -40,17 +16,22 @@ key is assigned the corresponding key (e.g., cv, dynamic, form). -->
 
         <!-- src is the image URL, and key is the key name (e.g., cv, dynamic, form). -->
         <!-- :key="key": Provides a unique key for each item in the loop, which helps Vue to optimize rendering. -->
-        <div v-for="(src, key) in imageSources" :key="key" class="image-sizing">
+        <div
+          v-for="(data, key) in imageSources"
+          :key="key"
+          class="image-sizing"
+        >
           <!-- This syntax binds the src attribute of the img element to the src
           variable from the v-for loop. This means each image will use the
           corresponding URL from the imageSources object.
           src is just a variable name used in the loop. You could indeed choose a different name if you wanted to. -->
           <img
-            :src="src"
+            :src="data.src"
             :alt="`${key} image`"
             @click="openModal(key)"
             class="image-size"
           />
+          <p class="image-text">{{ data.tag }}</p>
           <!-- :key attribute in the v-for loop is used to provide a unique identifier for each item in the loop.
            :alt="${key} image" uses a template string to include the key variable in the alt text, making it unique for each image. -->
 
@@ -78,17 +59,22 @@ key is assigned the corresponding key (e.g., cv, dynamic, form). -->
 <script>
 import DynamicModal from "@/components/MyModals.vue";
 import ContactForm from "@/components/ContactForm.vue";
+import AboutView from "./AboutView.vue";
 export default {
   components: {
     DynamicModal,
     ContactForm,
+    AboutView,
   },
   data() {
     return {
       imageSources: {
-        cv: "images/cv.jpg",
-        dynamic: "images/dynamic.jpg",
-        form: "images/form.jpg",
+        cv: { src: "images/cv.jpg", tag: "My CV project (Responsive)" },
+        dynamic: {
+          src: "images/dynamic.jpg",
+          tag: "HomeSpace (Not Responsive)",
+        },
+        form: { src: "images/form.jpg", tag: "Form project (Responsive)" },
       },
       modals: {
         cv: false,
@@ -119,47 +105,11 @@ body {
 }
 
 /*****************MAIN*************/
-.slider {
-  padding-top: 150px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: #000;
-}
-.slider-background {
-  width: 300px;
-  height: 300px;
-  object-fit: cover;
-  padding: 20px 50px 30px 50px;
-  filter: brightness(50%);
 
-  border-radius: 50%;
-}
-
-.slider-content {
-  width: 60%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-h1 {
-  color: greenyellow;
-  font-size: 3rem;
-  text-transform: uppercase;
-}
 h2 {
   font-size: 3rem;
   text-transform: uppercase;
   color: greenyellow;
-}
-.w20 {
-  padding-bottom: 10px;
-}
-.mz-11 {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
 }
 
 /* image sizing */
@@ -168,121 +118,54 @@ h2 {
   margin: 10px;
 }
 .image-size {
-  height: 200px;
-  width: 200px;
+  height: 250px;
+  width: 250px;
 }
-.me-2 {
-  height: 210px;
-  width: 210px;
-  object-fit: cover;
-  border: 5px solid;
-}
-.me-2:hover {
+
+.image-size:hover {
   box-shadow: 10px 10px 10px rgba(6, 240, 41, 0.514);
 }
-.slider-content p {
-  color: #fff;
-  font-size: 2rem;
-  font-weight: 400;
-  text-transform: uppercase;
-  color: #fff;
-  padding-top: 30px;
-}
-.about-me {
-  padding: 200px 100px 100px 100px;
+/* image-text */
+.image-text {
+  color: greenyellow;
+  padding-top: 10px;
   display: flex;
+  justify-content: center;
 }
-.d-10 {
-  background: #000;
+
+.me-2:hover {
+  box-shadow: 10px 10px 10px rgba(6, 240, 41, 0.514);
 }
 
 .w20 {
   width: 40%;
   display: flex;
-  align-items: center;
-}
-.w10 {
-  width: 60%;
-  color: #fff;
-  font-size: 1.5rem;
-  text-align: justify;
-  gap: 5px;
-  line-height: 50px;
 }
 
-.w10 strong {
-  color: greenyellow;
-  font-size: 2.5rem;
-}
-.mt-10 {
-  display: flex;
-  gap: 60px;
-}
-figure {
-  position: relative;
-}
-
-.fig-9 {
-  color: #fff;
-  text-transform: uppercase;
-  font-size: 0.9rem;
-  display: flex;
-  justify-content: center;
-}
 .exper {
   display: flex;
   background: #000;
-  padding: 200px 100px 200px 100px;
+  padding: 200px 100px 100px 100px;
 }
 .me {
   color: #fff;
 }
 
 @media (max-width: 810px) {
-  .slider {
-    padding: 20px 0 20px 0;
-  }
-  .slider-background {
-    width: 100px;
-    height: 100px;
-    padding: 20px;
-  }
-  h1 {
-    font-size: 1rem;
-  }
-  .slider-content p {
-    font-size: 0.7rem;
-  }
-  .about-me {
-    padding: 20px 15px 20px 15px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
   h2 {
     font-size: 1rem;
     padding-bottom: 10px;
   }
 
-  .w10 {
-    font-size: 0.7rem;
-    line-height: 25px;
+  .image-size {
+    height: 200px;
+    width: 200px;
   }
-  .w10 strong {
-    font-size: 0.8rem;
-    filter: brightness(60%);
-  }
-  .mt-10 {
-    gap: 10px;
-  }
+
   .exper {
     flex-direction: column;
     padding: 20px 10px 20px 10px;
     align-items: center;
-  }
-
-  .fig-9 {
-    padding: 10px 0 5px 0;
   }
 }
 </style>
